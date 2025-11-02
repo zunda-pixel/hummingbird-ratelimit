0e1daddaff1c0f124230184789a699a24cb15c51
zunda <47569369+zunda-pixel@users.noreply.github.com>
2025-11-03 00:56:30 +0900

    Add project (#1)

---
+++ Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "hummingbird-ratelimit",
+  platforms: [
+    .macOS(.v26),
+  ],
+  products: [
+    .library(
+      name: "RateLimit",
+      targets: ["RateLimit"]
+    ),
+    .executable(
+      name: "App",
+      targets: ["App"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "App",
+      dependencies: [
+        .target(name: "RateLimit"),
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ]
+    ),
+    .target(
+      name: "RateLimit",
+      dependencies: [
+        .product(name: "Hummingbird", package: "hummingbird"),
+      ]
+    ),
+    .testTarget(
+      name: "RateLimitTests",
+      dependencies: [
+        .target(name: "RateLimit"),
+        .product(name: "HummingbirdTesting", package: "hummingbird"),
+      ]
+    ),
+  ]
+)
---
+++ Sources/RateLimit/RateLimit.swift
@@ -0,0 +1,27 @@
+import Hummingbird
+
+public struct RateLimitMiddleware<Context: RequestContext>: RouterMiddleware {
+  public init() {}
+  
+  public func handle(
+    _ input: HummingbirdCore.Request,
+    context: Context,
+    next: (HummingbirdCore.Request, Context) async throws -> HummingbirdCore.Response
+  ) async throws -> HummingbirdCore.Response {
+    print(input.headers)
+    print(input.uri)
+    print(input.head)
+    
+    print(input.headers[.xForwardedFor])
+    print(input.headers[.cfConnectionIP])
+    
+    return try await next(input, context)
+  }
+}
+
+import HTTPTypes
+
+extension HTTPField.Name {
+  static let xForwardedFor = Self("X-Forwarded-For")!
+  static let cfConnectionIP = Self("CF-Connecting-IP")!
+}
